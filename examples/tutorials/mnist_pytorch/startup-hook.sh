7e8017df0f62d80d21f5483578e2d5abd0e30935
Prabhu Murthy <55988297+prabhum456@users.noreply.github.com>
2022-04-07 22:54:43 +0530

    Update startup-hook.sh

--- examples/tutorials/mnist_pytorch/startup-hook.sh
+++ examples/tutorials/mnist_pytorch/startup-hook.sh
@@ -1,3 +1,3 @@
 export http_proxy=http://172.29.6.250:8080
 export https_proxy=http://172.29.6.250:8080
-export no_proxy=10.104.41.208,10.110.88.18,172.29.57.233
+export no_proxy=10.104.41.208,10.110.88.18,172.29.57.233,172.29.57.112,glhc-cp-training-0.glhc-hpe.local
